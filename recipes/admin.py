a3e9041ec57b1df4c137e4609d6ecf33ae6b8a48
Alan-RiBas <alanbatista.ribas@gmail.com>
2023-08-06 21:13:14 -0300

    Add media folder path to them, def string method

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+# Outra forma de registrar no admin
+# admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Recipe, RecipeAdmin)
