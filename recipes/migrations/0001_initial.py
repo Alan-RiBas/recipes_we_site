0cd7c6861c61abf16472d1568c6c36ed94af652c
Alan-RiBas <alanbatista.ribas@gmail.com>
2023-08-06 21:49:35 -0300

    Creating category view

--- recipes/migrations/0001_initial.py
+++ recipes/migrations/0001_initial.py
@@ -17,29 +17,43 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True, serialize=False,
+                    verbose_name='ID'
+                )),
                 ('name', models.CharField(max_length=64)),
             ],
         ),
         migrations.CreateModel(
             name='Recipe',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True, serialize=False,
+                    verbose_name='ID'
+                )),
                 ('title', models.CharField(max_length=64)),
                 ('description', models.CharField(max_length=168)),
                 ('slug', models.SlugField()),
-                ('preparatio_time', models.IntegerField()),
-                ('preparatio_time_unit', models.CharField(max_length=16)),
+                ('preparation_time', models.IntegerField()),
+                ('preparation_time_unit', models.CharField(max_length=16)),
                 ('servings', models.IntegerField()),
                 ('servings_unit', models.CharField(max_length=16)),
-                ('preparagion_steps', models.TextField()),
-                ('preparagion_steps_is_html', models.BooleanField()),
+                ('preparation_steps', models.TextField()),
+                ('preparation_steps_is_html', models.BooleanField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('is_publushed', models.BooleanField(default=False)),
-                ('cover', models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')),
-                ('author', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
-                ('category', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='recipes.category')),
+                ('cover', models.ImageField(
+                    upload_to='recipes/covers/%Y/%m/%d/'
+                )),
+                ('author', models.ForeignKey(
+                    null=True, on_delete=django.db.models.deletion.SET_NULL,
+                    to=settings.AUTH_USER_MODEL
+                )),
+                ('category', models.ForeignKey(
+                    null=True, on_delete=django.db.models.deletion.SET_NULL,
+                    to='recipes.category'
+                )),
             ],
         ),
     ]
--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'recipes'
 urlpatterns = [
     path('', views.home, name="home"),
-    path('recipes/<slug:id>/', views.recipe, name="recipe"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
