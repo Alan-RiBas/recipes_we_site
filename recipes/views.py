590ac36a08d8e675cfda323116f22679b57a9b13
Alan-RiBas <alanbatista.ribas@gmail.com>
2023-09-30 22:43:06 -0300

    adicionando error 404" "

--- recipes/views.py
+++ recipes/views.py
@@ -1,22 +1,19 @@
-# from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter(
         is_published=True
-    ).order_by('id')
+        ).order_by('id'))
+    
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes
     })
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        id=id,
-        is_published=True
-        ).first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_datail_page': True,
@@ -24,10 +21,12 @@ def recipe(request, id):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id,
         is_published=True
-        ).order_by('id')
+        ).order_by('id'))
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category'
     })
