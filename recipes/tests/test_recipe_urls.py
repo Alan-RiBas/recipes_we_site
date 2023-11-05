6ca9cc108df4e18a03752386220ba79f47ebe194
Alan-RiBas <alanbatista.ribas@gmail.com>
2023-11-05 19:16:38 -0300

    Separando testes.

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -7,11 +7,11 @@ class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
         home_url = reverse('recipes:home')
         self.assertEqual(home_url, '/')
-    
+
     def test_recipe_url_is_correct(self):
         home_url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(home_url, '/recipes/1/')
-    
+
     def test_recipe_category_url_is_correct(self):
         home_url = reverse('recipes:category', kwargs={'category_id': 1})
         self.assertEqual(home_url, '/recipes/category/1/')
